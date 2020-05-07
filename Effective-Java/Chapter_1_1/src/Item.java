f3d3dbb2433ef0653ff90271ed12ecb0f80b754d
wyjax <wyjax.dev@gmail.com>
2020-05-07 10:41:19 +0900

    정적 팩터리 메서드 내용 보강

--- Effective-Java/Chapter_1_1/src/Item.java
+++ Effective-Java/Chapter_1_1/src/Item.java
@@ -1,26 +1,58 @@
 class Person {
     private static Person person = new Person();
     private String name = "default";
+    private int age = 0;
 
-    // 기본 private 생성자
     private Person() {
 
     }
 
-    // name 초기화 private 생성자
+    // name을 초기화하는 생성자
     private Person(String name) {
         this.name = name;
     }
 
+    // age로 초기화하는 생성자
+    private Person(Integer age) {
+        this.age = age;
+    }
+
+    private Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // from by name(string)
+    public Person fromByName(String name) {
+        return new Person(name);
+    }
+
+    // from by age(integer)
+    public Person fromByAge(int age) {
+        return new Person(age);
+    }
+
+    // of : 여러 매개변수로 생성
+    public Person of(String name, int age) {
+        return new Person(name, age);
+    }
+
+    public Person valueOf(String name, int age) {
+        return new Person(name, age);
+    }
+
+    // 기존 인스턴스 return
     public static Person getInstance() {
         return person;
     }
 
+    // new 인스턴스 return
     public synchronized static Person newInstance() {
         person = new Person();
         return person;
     }
 
+    // 멀티 스테드 동기화
     public synchronized static Person newInstanceByName(String name) {
         person = new Person(name);
         return person;
--- Effective-Java/Chapter_1_1/src/cp_1_1.java
+++ Effective-Java/Chapter_1_1/src/cp_1_1.java
@@ -1,18 +1,30 @@
 
 public class cp_1_1 {
     public static void main(String[] args) {
-        ChampionModel rise = Champion.create("Rize");
-        ChampionModel ezreal = Champion.create("Ezreal");
+        ChampionModel ryze1 = Champion.create("Rize");
+        ChampionModel ezreal1 = Champion.create("Ezreal");
+
+        ChampionModel ryze2 = Champion.create("Rize");
+        ChampionModel ezreal2 = Champion.create("Ezreal");
+
+        System.out.println("ryze object 비교 : " + (ryze1.equals(ryze2)));
+        System.out.println("ezreal object 비교 : " + (ezreal1.equals(ezreal2)));
     }
 }
 
 class Champion {
+    private static class LazyHolder {
+        private static final Ryze RYZE = new Ryze(575, 300, 50, 100);
+        private static final Ezreal EZREAL = new Ezreal(500, 375, 30, 90);
+    }
+
+    // 하위 자료형 객체를 반환할 수 있다.
     public static ChampionModel create(String championName) {
         if (championName.equals("Rize")) {
-            return new Rize(100, 100, 10, 10);
+            return LazyHolder.RYZE;
         }
         else if (championName.equals("Ezreal")) {
-            return new Ezreal(100, 100, 10, 10);
+            return LazyHolder.EZREAL;
         }
         return null;
     }
@@ -24,13 +36,13 @@ interface ChampionModel {
     public void userR();
 }
 
-class Rize implements ChampionModel {
+class Ryze implements ChampionModel {
     private int healthPoint;
     private int magicPoint;
     private int qPay;
     private int rPay;
 
-    public Rize(int healthPoint, int magicPoint, int qPay, int rPay) {
+    public Ryze(int healthPoint, int magicPoint, int qPay, int rPay) {
         this.healthPoint = healthPoint;
         this.magicPoint = magicPoint;
         this.qPay = qPay;
