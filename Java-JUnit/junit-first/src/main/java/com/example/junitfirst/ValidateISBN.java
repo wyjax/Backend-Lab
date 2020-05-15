a23b9065f8ee0aac3ee3192485209e44fb580f40
wyjax <wyjax.dev@gmail.com>
2020-05-15 23:53:00 +0900

    TDD - 2

--- Java-JUnit/junit-first/src/main/java/com/example/junitfirst/ValidateISBN.java
+++ Java-JUnit/junit-first/src/main/java/com/example/junitfirst/ValidateISBN.java
@@ -2,12 +2,20 @@ package com.example.junitfirst;
 
 public class ValidateISBN {
 
-
-    public boolean checkISBN(int isbn) {
-        if (isbn == 140449116) {
-            return true;
+    public boolean checkISBN(String ISBN) {
+        if (ISBN.length() != 10) {
+            throw new NumberFormatException("ISBN의 수가 10자리가 아닙니다.");
         }
 
+        int total = 0;
+
+        for (int i = 0; i < 10; i++) {
+            total += ISBN.charAt(i) * (10 - i);
+        }
+
+        if (total % 11 == 0) {
+            return true;
+        }
         return false;
     }
 }
