f77f015c4edd0df28d588be7f86d833f2810116d
wyjax <wyjax.dev@gmail.com>
2020-08-09 23:23:33 +0900

    spring boot 멀티모듈 구조 만들기 연습

---
+++ multimodule/module-web/src/main/java/com/wyjax/ModuleWebApplication.java
@@ -0,0 +1,13 @@
+package com.wyjax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleWebApplication {
+    public static void main(String[] args) {
+        System.out.println();
+
+        SpringApplication.run(ModuleWebApplication.class, args);
+    }
+}
