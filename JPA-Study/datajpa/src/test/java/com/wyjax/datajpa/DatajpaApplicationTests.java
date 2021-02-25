75806a89d58a43fbd53e9fc0fbfa889ec2ef33cd
wyjax <wyjax.dev@gmail.com>
2021-02-25 22:46:48 +0900

    JPA Study initial Commit

---
+++ JPA-Study/datajpa/src/test/java/com/wyjax/datajpa/DatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyjax.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatajpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
