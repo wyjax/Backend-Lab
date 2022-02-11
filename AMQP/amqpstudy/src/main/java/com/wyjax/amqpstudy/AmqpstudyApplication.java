601a13bf30e39fc570ec8616f6b5f2d09cde9409
wyjax <wyjax.dev@gmail.com>
2022-02-11 22:33:02 +0900

    TIL: init amqp study project

---
+++ AMQP/amqpstudy/src/main/java/com/wyjax/amqpstudy/AmqpstudyApplication.java
@@ -0,0 +1,13 @@
+package com.wyjax.amqpstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqpstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmqpstudyApplication.class, args);
+	}
+
+}
---
+++ AMQP/amqpstudy/src/test/java/com/wyjax/amqpstudy/AmqpstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyjax.amqpstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqpstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
