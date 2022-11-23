2a676ad31bab560d8541843b069c228c1930ff7d
wyjax <wyjax.dev@gmail.com>
2022-11-23 22:49:58 +0900

    refactor: 불필요 삭제

--- oauth2-jwt/src/main/java/com/wyjax/oauth2jwt/config/filter/OAuth2AuthenticationFailureHandler.java
+++
@@ -1,8 +0,0 @@
-package com.wyjax.oauth2jwt.config.filter;
-
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-
-public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
-
-
-}
