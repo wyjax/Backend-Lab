0e0dacc5d55885d7f4ed5d64f983c32218c2e42d
wyjax <wyjax.dev@gmail.com>
2022-11-24 00:20:09 +0900

    feat: security config 새로운 버전

--- sociallogin/src/main/java/com/wyjax/sociallogin/security/SecurityConfig.java
+++ sociallogin/src/main/java/com/wyjax/sociallogin/security/SecurityConfig.java
@@ -1,5 +1,35 @@
 package com.wyjax.sociallogin.security;
 
-public class SecurityConfig {
+import com.wyjax.sociallogin.security.component.OAuth2AuthenticationSuccessHandler;
+import com.wyjax.sociallogin.security.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig {
+    private final CustomOAuth2UserService oAuth2UserService;
+    private final OAuth2AuthenticationSuccessHandler successHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests()
+                .antMatchers("/**")
+                .permitAll();
+        http.headers()
+                .frameOptions()
+                .sameOrigin();
+        http.oauth2Login()
+                .userInfoEndpoint()
+                .userService(oAuth2UserService)
+                .and()
+                .successHandler(successHandler);
+        return http.build();
+    }
 }
