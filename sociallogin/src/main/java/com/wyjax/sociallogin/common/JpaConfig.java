d63d17cc22a48d3cf370dc477f6f7e90cdbbb652
wyjax <wyjax.dev@gmail.com>
2022-11-24 00:20:21 +0900

    feat: member domain

---
+++ sociallogin/src/main/java/com/wyjax/sociallogin/common/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.wyjax.sociallogin.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
